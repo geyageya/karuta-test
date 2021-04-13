4bfb823a0f1d989930f2b2702c1e2cd735ceee4b
geyageya <74279452+geyageya@users.noreply.github.com>
2021-04-14 07:52:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@
       image.classList.add("erase"); */
       
       //機能しない All付き
-      const image = document.querySelectorAll("img[src+karuta[no].efuda]");
+      const image = document.querySelector("img[src+karuta[no].efuda]");
       image.classList.add("erase");  
       //機能しない All無し
       /* const image = document.querySelector("img[src ='"+karuta[no].efuda+"']");
